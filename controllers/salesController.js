b5a4ddbddd8cefecf45bfcc4c75f2e7ab0e9f5e7
Iury Roque <iury.roque@hotmail.com>
2022-04-06 20:21:01 -0300

    Feat: Create salesController getById

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -8,3 +8,20 @@ const getAll = async (req, res, next) => {
     next(error);
   }
 };
+
+const getById = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const sale = await salesService.getById(id);
+
+    if (sale.length === 0) return res.status(404).json({ message: 'Sale not found' });
+    return res.status(200).json(sale);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  getAll,
+  getById,
+};
