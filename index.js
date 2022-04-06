d50bf2a8fb96b865e469b3dc6f27fd8d187bd039
Iury Roque <iury.roque@hotmail.com>
2022-04-06 20:22:28 -0300

    Feat: Import validate middleware

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const productsController = require('./controllers/productsController');
 const salesController = require('./controllers/salesController');
+const validate = require('./middlewares/validate');
 const joi = require('./Schemas/validateId');
 
 const app = express();
@@ -12,9 +13,13 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.post('/products',
+  validate.joiValidate(joi.name), validate.checkProductExists, productsController.create);
 app.get('/products/:id', productsController.getById);
 app.get('/products', productsController.getAll);
 
+// app.post('/products', productsController.create);
 app.get('/sales/:id', salesController.getById);
 app.get('/sales', salesController.getAll);
 
