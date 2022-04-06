f327fd8ede6370b645c27d617bd4436af6f680c0
Iury Roque <iury.roque@hotmail.com>
2022-04-06 20:25:45 -0300

    Feat: Create middleware checkProductExist in DataB

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -1,3 +1,5 @@
+const validateService = require('../services/validateService');
+
 // middleware que chama schema validação do joi
 const joiValidate = (schema) => (req, res, next) => {
 const { error } = schema.validate(req.body);
@@ -8,6 +10,15 @@ if (error) {
 next();
 };
 
+const checkProductExists = async (req, res, next) => {
+  const { name } = req.body;
+  const productExists = await validateService.checkProductExists(name);
+   // Se o nome já existir no banco o service retornar productExists com true ou false caso não exista
+   if (productExists) return res.status(409).json({ message: 'Product already exists' });
+   next();
+};
+
 module.exports = {
   joiValidate,
+  checkProductExists,
 };
