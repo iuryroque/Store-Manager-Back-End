15e5cf3c345ec315f2f66843c0e711780b98f80b
Iury Roque <iury.roque@hotmail.com>
2022-04-06 20:15:22 -0300

    Feat: Create salesService getAll

---
+++ services/salesService.js
@@ -0,0 +1,10 @@
+const salesModel = require('../models/salesModel');
+
+const getAll = async () => {
+  const sales = await salesModel.getAll();
+  return sales;
+};
+
+module.exports = {
+  getAll,
+};
