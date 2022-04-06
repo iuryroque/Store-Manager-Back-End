93f9283f5e69f6793a33f484e31239132e59575c
Iury Roque <iury.roque@hotmail.com>
2022-04-06 20:14:22 -0300

    Feat: Create salesModels serialize

--- models/salesModel.js
+++ models/salesModel.js
@@ -1,5 +1,12 @@
 const connection = require('./connection');
 
+const serialize = (salesData) => ({
+  saleId: salesData.sale_id,
+  date: salesData.date,
+  productId: salesData.product_id,
+  quantity: salesData.quantity,
+});
+
 const getAll = async () => {
   const [sales] = await connection.execute(`SELECT sale_id, product_id, s.date, quantity
   FROM sales s INNER JOIN sales_products sp
