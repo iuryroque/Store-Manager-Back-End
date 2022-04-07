0ef6386a7eb6aa05363bd1d386c8e904bb333e81
Iury Roque <iury.roque@hotmail.com>
2022-04-07 20:14:15 -0300

    Agradecimento ao rafa reis

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 
+// Um agradecimento para o Rafa Reis da trybe por as muitas ajudas mas em especial por me ajuda a montar um estrutura mais 'modularizada' das validações;
+
 const productsController = require('./controllers/productsController');
 const salesController = require('./controllers/salesController');
 const validate = require('./middlewares/validate');
