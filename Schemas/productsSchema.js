f8065b0c65933b7f4f2b30174c25cb9ac36f24e2
Iury Roque <iury.roque@hotmail.com>
2022-04-09 19:48:41 -0300

    Fix: Adjusts productSchema message name error (\")

--- Schemas/productsSchema.js
+++ Schemas/productsSchema.js
@@ -3,13 +3,13 @@ const joi = require('joi');
 module.exports = joi.object({
   name: joi.string().required().empty().min(5)
     .messages({
-    'any.required': '400|name is required',
-    'string.min': '422|name length must be at least 5 characters long',
-    'string.empty': '409|Product already exists',
+    'any.required': '400|\"name\" is required',
+    'string.min': '422|\"name\" length must be at least 5 characters long',
+    'string.empty': '409|\"Product\" already exists',
   }),
   quantity: joi.number().required().empty().min(1)
     .messages({
-    'number.min': '422|quantity must be greater than or equal to 1',
-    'any.required': '400|quantity is required',
+    'number.min': '422|\"quantity\" must be greater than or equal to 1',
+    'any.required': '400|\"quantity\" is required',
   }),
 });
