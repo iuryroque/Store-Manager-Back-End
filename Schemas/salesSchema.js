5bc387eefad2942074a939251b550e3d40b14935
Iury Roque <iury.roque@hotmail.com>
2022-04-09 20:29:42 -0300

    Fix: Lint Errors

--- Schemas/salesSchema.js
+++ Schemas/salesSchema.js
@@ -3,14 +3,14 @@ const joi = require('joi');
 module.exports = joi.object({
   productId: joi.number().required().empty().min(1)
     .messages({
-    'number.min': '422|\"quantity\" be greater than or equal to 1',
-    'any.required': '400|\"productId\" is required',
-    'number.empty': '409|\"Product\" already exists',
+    'number.min': '422|\quantity\ be greater than or equal to 1',
+    'any.required': '400|\productId\ is required',
+    'number.empty': '409|\Product\ already exists',
   }),
   quantity: joi.number().required().empty().min(1)
     .messages({
-    'number.min': '422|\"quantity\" must be greater than or equal to 1',
-    'any.required': '400|\"quantity\" is required',
-    'number.empty': '409|\"Product\" already exists',
+    'number.min': '422|\quantity\ must be greater than or equal to 1',
+    'any.required': '400|\quantity\ is required',
+    'number.empty': '409|\Product\ already exists',
   }),
 });
