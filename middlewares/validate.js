8b1239372ec1ca92a3ab807d80731acae2ad9d22
Iury Roque <iury.roque@hotmail.com>
2022-04-09 19:51:12 -0300

    Feat: Adjust joivalidate for received array

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -2,7 +2,16 @@ const validateService = require('../services/validateService');
 
 // middleware que chama schema validação do joi
 const joiValidate = (schema) => (req, res, next) => {
-const { error } = schema.validate(req.body);
+  let error;
+
+  // Validação nessecaria quando o corpo da requisição é um array
+  if (req.body[0]) {
+    req.body.forEach((sale) => {
+      ({ error } = schema.validate(sale));
+    });
+  } else {
+    ({ error } = schema.validate(req.body));
+  }
 if (error) {
   const [code, message] = error.message.split('|');
   return res.status(code).json({ message });
