9b8d0326533249e2640755d60d502395737e425a
Iury Roque <iury.roque@hotmail.com>
2022-04-09 20:12:46 -0300

    Feat: Creat service for add sale

--- services/salesService.js
+++ services/salesService.js
@@ -10,7 +10,33 @@ const getById = async (id) => {
   return sale;
 };
 
+// Agradecimento ao Leandro Oliveira e ao Lucas Pinheiro ambos da t15b por a conversa sobre promisse e qual a melhor forma de fazer "map ou foreach...".
+const create = async (saleList) => {
+  const { insertId } = await salesModel.createSaleNow();
+
+  const saleNewList = { id: insertId, itemsSold: [] };
+
+  await Promise.all(saleList.map(
+    async ({ productId, quantity }) => {
+      await salesModel.createProductSale(insertId, productId, quantity);
+      saleNewList.itemsSold.push({ productId, quantity });
+    },
+  ));
+
+  return saleNewList;
+  // Criado para fazer uma venda por item da lista
+  // const saleNewList = [];
+  // await Promise.all(saleList.map(
+  //   async ({ productId, quantity }) => {
+  //     const { insertId } = await salesModel.createSaleNow();
+  //     await salesModel.createSale(insertId, productId, quantity);
+  //     saleNewList.push({ id: insertId, productId, quantity });
+  //   },
+  // ));
+};
+
 module.exports = {
   getAll,
   getById,
+  create,
 };
