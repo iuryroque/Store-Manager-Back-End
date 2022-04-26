69c3fa7a9e8b2da984d6544ea148cbfc1db961e0
Iury Roque <iury.roque@hotmail.com>
2022-04-26 20:10:29 -0300

    Feat: Cria service edit e destroy, chama calculos

--- services/salesService.js
+++ services/salesService.js
@@ -1,4 +1,5 @@
 const salesModel = require('../models/salesModel');
+const calculations = require('../calculations');
 
 const getAll = async () => {
   const sales = await salesModel.getAll();
@@ -13,9 +14,7 @@ const getById = async (id) => {
 // Agradecimento ao Leandro Oliveira e ao Lucas Pinheiro ambos da t15b por a conversa sobre promisse e qual a melhor forma de fazer "map ou foreach...".
 const create = async (saleList) => {
   const { insertId } = await salesModel.createSaleNow();
-
   const saleNewList = { id: insertId, itemsSold: [] };
-
   await Promise.all(saleList.map(
     async ({ productId, quantity }) => {
       await salesModel.createProductSale(insertId, productId, quantity);
@@ -23,20 +22,40 @@ const create = async (saleList) => {
     },
   ));
 
+  await calculations.sub(saleList);
   return saleNewList;
   // Criado para fazer uma venda por item da lista
   // const saleNewList = [];
   // await Promise.all(saleList.map(
   //   async ({ productId, quantity }) => {
-  //     const { insertId } = await salesModel.createSaleNow();
-  //     await salesModel.createSale(insertId, productId, quantity);
+    //     const { insertId } = await salesModel.createSaleNow();
+    //     await salesModel.createSale(insertId, productId, quantity);
   //     saleNewList.push({ id: insertId, productId, quantity });
   //   },
   // ));
 };
 
-module.exports = {
-  getAll,
-  getById,
-  create,
+const edit = async (id, saleList) => {
+  const saleEditList = { saleId: id, itemUpdated: saleList };
+  await calculations.sum(id);
+  await calculations.sub(saleList);
+  await Promise.all(saleList.map(
+    async ({ productId, quantity }) => salesModel.edit(id, productId, quantity)
+    ,
+    ));
+    return saleEditList;
+  };
+
+  const destroy = async (id) => {
+    await calculations.sum(id);
+    const retorno = await salesModel.destroy(id);
+    return retorno;
+  };
+
+  module.exports = {
+    getAll,
+    getById,
+    create,
+  edit,
+  destroy,
 };
