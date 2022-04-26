d9e6d1d9f5501d6317f142caaa0543c43a553967
Iury Roque <iury.roque@hotmail.com>
2022-04-26 20:09:36 -0300

    Feat: Cria controller edit e destroy

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -22,8 +22,8 @@ const getById = async (req, res, next) => {
 };
 
 const create = async (req, res, next) => {
+  const saleList = req.body;
   try {
-    const saleList = req.body;
     const resSaleList = await salesService.create(saleList);
     return res.status(201).json(resSaleList);
   } catch (error) {
@@ -33,7 +33,20 @@ const create = async (req, res, next) => {
 
 const edit = async (req, res, next) => {
   try {
-    return res.status(201).json();
+    const { id } = req.params;
+    const saleList = req.body;
+    const b = await salesService.edit(id, saleList);
+    return res.status(200).json(b);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const destroy = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await salesService.destroy(id);
+    return res.status(204).end();
   } catch (error) {
     next(error);
   }
@@ -44,4 +57,5 @@ module.exports = {
   getById,
   create,
   edit,
+  destroy,
 };
